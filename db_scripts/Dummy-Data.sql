6625c6f5a43b4891f492784dbfaddf3e6befbf02
DominikHeinzer <dominik13@gmx.ch>
2017-05-10 05:50:27 -0700

    added percentage values to the Dummy-Data

--- db_scripts/Dummy-Data.sql
+++ db_scripts/Dummy-Data.sql
@@ -8,4 +8,4 @@ Insert into user(`name`, firstname, `password`, email, fk_id_group) values ('sie
 
 Insert into messages(`message_text`, fk_id_user_from, fk_id_user_to) values ('Hello World', 1, 2 );
 Insert into modul(modulname, modulnummer) values ('Software mit agilen Methoden', '426' );
-Insert into user_modul(lb, zp1, zp2, mj, fk_id_user, fk_id_modul) values (5.3, 3.5, 4.4, 6.0 , 2, 1);
+Insert into user_modul(lb, zp1, zp2, mj, percentage_lb, percentage_zp1, percentage_zp2, percentage_mj, fk_id_user, fk_id_modul) values (5.3, 3.5, 4.4, 6.0 , 50, 15, 15, 20, 2, 1);
