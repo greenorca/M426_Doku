82703ffbaea219e9035d2b353470482a0dd5597e
ezrarieben <ezra.rieben@gmail.com>
2017-05-10 04:07:31 -0700

    Removed unused project folder

--- project_sierra/src/view.template/head.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-	echo "<link href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css' type='text/css' rel='stylesheet'>
-		  <link href='view.template/css/style.css' type='text/css' rel='stylesheet'>
-		  <meta charset='UTF-8'>
-		"
-?>
