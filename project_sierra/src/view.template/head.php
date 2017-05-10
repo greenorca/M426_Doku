99e9dbbc13a6c7f296c029c169d1849629f63aa4
Jäger Sascha <sascha.jaeger@wiss-online.ch>
2017-05-10 11:32:04 +0200

    Created Project-Folder and general page-structure

---
+++ project_sierra/src/view.template/head.php
@@ -0,0 +1,7 @@
+<?php
+
+	echo "<link href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css' type='text/css' rel='stylesheet'>
+		  <link href='view.template/css/style.css' type='text/css' rel='stylesheet'>
+		  <meta charset='UTF-8'>
+		"
+?>
