a5ba44d00f1285f1d41b5c299ed6e1a848f6d5e1
Ezra Rieben <ezra.rieben@gmail.com>
2017-05-23 20:37:12 +0200

    Updated SQL Script to fit naming conventions
    Naming conventions can be seen here: https://launchbylunch.com/posts/2014/Feb/16/sql-naming-conventions/#naming-conventions

--- db_scripts/Sierra-Db-Script.sql
+++ db_scripts/Sierra-Db-Script.sql
@@ -10,82 +10,82 @@ CREATE SCHEMA IF NOT EXISTS `Project-Sierra` DEFAULT CHARACTER SET utf8 COLLATE 
 USE `Project-Sierra` ;
 
 -- -----------------------------------------------------
--- Table `Project-Sierra`.`Groups`
+-- Table `Project-Sierra`.`group`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Project-Sierra`.`Groups` (
-  `idGroups` INT NOT NULL AUTO_INCREMENT,
-  `Groupname` VARCHAR(45) NULL,
-  PRIMARY KEY (`idGroups`))
+CREATE TABLE IF NOT EXISTS `Project-Sierra`.`group` (
+  `group_id` INT NOT NULL AUTO_INCREMENT,
+  `group_name` VARCHAR(45) NULL,
+  PRIMARY KEY (`group_id`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Project-Sierra`.`User`
+-- Table `Project-Sierra`.`user`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Project-Sierra`.`User` (
-  `idUser` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NULL,
-  `firstname` VARCHAR(45) NULL,
-  `password` VARCHAR(45) NULL,
-  `email` VARCHAR(45) NULL,
+CREATE TABLE IF NOT EXISTS `Project-Sierra`.`user` (
+  `user_id` INT NOT NULL AUTO_INCREMENT,
+  `last_name` VARCHAR(45) NULL,
+  `first_name` VARCHAR(60) NULL,
+  `password` VARCHAR(60) NULL,
+  `email` VARCHAR(55) NULL,
   `is_admin` TINYINT(1) NULL,
-  `fk_id_group` INT NULL,
-  PRIMARY KEY (`idUser`),
-  INDEX `fk_id_group_idx` (`fk_id_group` ASC),
-  CONSTRAINT `fk_id_group`
-    FOREIGN KEY (`fk_id_group`)
-    REFERENCES `Project-Sierra`.`Groups` (`idGroups`)
+  `fk_group_id` INT NULL,
+  PRIMARY KEY (`user_id`),
+  INDEX `fk_group_id_idx` (`fk_group_id` ASC),
+  CONSTRAINT `fk_group_id`
+    FOREIGN KEY (`fk_group_id`)
+    REFERENCES `Project-Sierra`.`group` (`group_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Project-Sierra`.`Messages`
+-- Table `Project-Sierra`.`message`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Project-Sierra`.`Messages` (
-  `idMessages` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `Project-Sierra`.`message` (
+  `message_id` INT NOT NULL AUTO_INCREMENT,
   `message_text` VARCHAR(250) NOT NULL,
-  `fk_id_user_from` INT NULL,
-  `fk_id_user_to` INT NULL,
+  `fk_user_id_from` INT NULL,
+  `fk_user_id_to` INT NULL,
   `fk_group_to` INT NULL,
-  PRIMARY KEY (`idMessages`),
-  INDEX `fk_id_user_from_idx` (`fk_id_user_from` ASC),
-  INDEX `fk_id_user_to_idx` (`fk_id_user_to` ASC),
-  INDEX `fk_id_group_to_idx` (`fk_group_to` ASC),
-  CONSTRAINT `fk_id_user_from`
-    FOREIGN KEY (`fk_id_user_from`)
-    REFERENCES `Project-Sierra`.`User` (`idUser`)
+  PRIMARY KEY (`message_id`),
+  INDEX `fk_user_id_from_idx` (`fk_user_id_from` ASC),
+  INDEX `fk_user_id_to_idx` (`fk_user_id_to` ASC),
+  INDEX `fk_group_id_to_idx` (`fk_group_to` ASC),
+  CONSTRAINT `fk_user_id_from`
+    FOREIGN KEY (`fk_user_id_from`)
+    REFERENCES `Project-Sierra`.`user` (`user_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_id_user_to`
-    FOREIGN KEY (`fk_id_user_to`)
-    REFERENCES `Project-Sierra`.`User` (`idUser`)
+  CONSTRAINT `fk_user_id_to`
+    FOREIGN KEY (`fk_user_id_to`)
+    REFERENCES `Project-Sierra`.`user` (`user_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_id_group_to`
+  CONSTRAINT `fk_group_id_to`
     FOREIGN KEY (`fk_group_to`)
-    REFERENCES `Project-Sierra`.`Groups` (`idGroups`)
+    REFERENCES `Project-Sierra`.`group` (`group_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Project-Sierra`.`Modul`
+-- Table `Project-Sierra`.`modul`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Project-Sierra`.`Modul` (
-  `idModul` INT NOT NULL AUTO_INCREMENT,
-  `modulname` VARCHAR(45) NULL,
+CREATE TABLE IF NOT EXISTS `Project-Sierra`.`modul` (
+  `modul_id` INT NOT NULL AUTO_INCREMENT,
+  `modullast_name` VARCHAR(45) NULL,
   `modulnummer` VARCHAR(4) NULL,
-  PRIMARY KEY (`idModul`))
+  PRIMARY KEY (`modul_id`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Project-Sierra`.`User_Modul`
+-- Table `Project-Sierra`.`user_modul`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Project-Sierra`.`User_Modul` (
+CREATE TABLE IF NOT EXISTS `Project-Sierra`.`user_modul` (
   `lb` DOUBLE NULL,
   `zp1` DOUBLE NULL,
   `zp2` DOUBLE NULL,
@@ -94,18 +94,18 @@ CREATE TABLE IF NOT EXISTS `Project-Sierra`.`User_Modul` (
   `percentage_zp1` INT NULL,
   `percentage_zp2` INT NULL,
   `percentage_mj` INT NULL,
-  `fk_id_user` INT NULL,
-  `fk_id_modul` INT NULL,
-  INDEX `fk_id_user_idx` (`fk_id_user` ASC),
-  INDEX `fk_id_modul_idx` (`fk_id_modul` ASC),
-  CONSTRAINT `fk_id_user`
-    FOREIGN KEY (`fk_id_user`)
-    REFERENCES `Project-Sierra`.`User` (`idUser`)
+  `fk_user_id` INT NULL,
+  `fk_modul_id` INT NULL,
+  INDEX `fk_user_id_idx` (`fk_user_id` ASC),
+  INDEX `fk_modul_id_idx` (`fk_modul_id` ASC),
+  CONSTRAINT `fk_user_id`
+    FOREIGN KEY (`fk_user_id`)
+    REFERENCES `Project-Sierra`.`user` (`user_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_id_modul`
-    FOREIGN KEY (`fk_id_modul`)
-    REFERENCES `Project-Sierra`.`Modul` (`idModul`)
+  CONSTRAINT `fk_modul_id`
+    FOREIGN KEY (`fk_modul_id`)
+    REFERENCES `Project-Sierra`.`modul` (`modul_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
