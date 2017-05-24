cf81d55e5428ebac105402f87d7d88a4c23b4e79
Richard Hofmaenner <richard.hofmaenner@bluewin.ch>
2017-05-24 01:11:33 -0700

    Copied DbController.php

--- src/controller/DbController.php
+++ src/controller/DbController.php
@@ -1,49 +1,21 @@
 <?php
-<<<<<<< HEAD
-class DbController{
+class DbController
+{
     const DBIP = "localhost";
-
     const DBNAME = "Project-Sierra";
-
     const DBUSER ="sierra";
     const DBPW = "sierra";
     private $dbconn;
-
-    function __construct(){
-
-        $this->dbconn = new mysqli(Self::DBIP,Self::DBUSER,Self::DBPW,Self::DBNAME);
+    public function __construct()
+    {
+        $this->dbconn = new mysqli(Self::DBIP, Self::DBUSER, Self::DBPW, Self::DBNAME);
         if ($this->dbconn->connect_error) {
             die("Database connection failed");
         }
     }
-
-    public function getDbconn(){
+    public function getDbconn()
+    {
         return $this->dbconn;
     }
 }
-=======
-    class DbController
-    {
-        const DBIP = "localhost";
-
-        const DBNAME = "Project-Sierra";
-
-        const DBUSER ="sierra";
-        const DBPW = "sierra";
-        private $dbconn;
-
-        public function __construct()
-        {
-            $this->dbconn = new mysqli(Self::DBIP, Self::DBUSER, Self::DBPW, Self::DBNAME);
-            if ($this->dbconn->connect_error) {
-                die("Database connection failed");
-            }
-        }
-
-        public function getDbconn()
-        {
-            return $this->dbconn;
-        }
-    }
->>>>>>> 503ebe50df83261c97d2138a0af20d2c82a61a39
 ?>
