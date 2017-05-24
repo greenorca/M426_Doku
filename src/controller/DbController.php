aa46e6165ca4af561a0e2a4bfa8faf171fadec1f
Richard Hofmaenner <richard.hofmaenner@bluewin.ch>
2017-05-24 02:43:43 -0700

    Merge conflict part 7

--- src/controller/DbController.php
+++ src/controller/DbController.php
@@ -1,25 +1,4 @@
 <?php
-<<<<<<< HEAD
-class DbController
-{
-    const DBIP = "localhost";
-    const DBNAME = "Project-Sierra";
-    const DBUSER ="sierra";
-    const DBPW = "sierra";
-    private $dbconn;
-    public function __construct()
-    {
-        $this->dbconn = new mysqli(Self::DBIP, Self::DBUSER, Self::DBPW, Self::DBNAME);
-        if ($this->dbconn->connect_error) {
-            die("Database connection failed");
-        }
-    }
-    public function getDbconn()
-    {
-        return $this->dbconn;
-    }
-}
-=======
     class DbController
     {
         const DBIP = "localhost";
@@ -43,5 +22,4 @@ class DbController
             return $this->dbconn;
         }
     }
->>>>>>> sprint-01
 ?>
