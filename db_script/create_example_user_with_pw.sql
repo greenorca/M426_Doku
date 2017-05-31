69eed0f767aa6659384c1e4dcc9cbbf221ffed40
Richard Hofmänner <richard.hofmaenner@bluewin.ch>
2017-05-31 09:09:45 +0200

    Removed db_script

--- db_script/create_example_user_with_pw.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `user` (`user_id`, `last_name`, `first_name`, `password`, `email`, `is_admin`, `fk_group_id`) VALUES
-(5, 'Doe', 'John', '$2a$14$XUulUYkomhL0eu.ERfxWTeFBLUGxRxZNmh2mX4t09/byBAkvAQ5hi', 'john.doe@example.com', 0, 2);
